af8c8dbeced80b9a3f99e668136aa081daee0963
Lindsay Clark <dragonzuela04@gmail.com>
2019-01-26 22:31:20 -0600

    comments explaining variance components (for my benefit)

--- hetability fucntion .R
+++ hetability fucntion .R
@@ -10,8 +10,8 @@ heritability <- function(x, y){
   Year = as.factor(y$Year)
   
   Misvarcomp <- lmer(x ~ (1|Entry) + (1|Rep) + (1|Year))
-  A = unlist(VarCorr(Misvarcomp))[[1]]
-  B = attr(VarCorr(Misvarcomp), "sc") ^ 2
+  A = unlist(VarCorr(Misvarcomp))[[1]]    # variance attributable to entry
+  B = attr(VarCorr(Misvarcomp), "sc") ^ 2 # residual variance
   h2 = A/(A+B/8)
   ####question: I used 8 as the number of Var_residual devided, but i am not sure, please let me know if it is not right. Thanks.
   return(h2)
