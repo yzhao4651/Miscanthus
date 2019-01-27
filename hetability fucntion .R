59a8d1f725045ca9c9b060ac8aa9648a9e0ae69c
Lindsay Clark <dragonzuela04@gmail.com>
2019-01-26 22:06:36 -0600

    white space for readability

--- hetability fucntion .R
+++ hetability fucntion .R
@@ -1,16 +1,16 @@
 
 ### write funcion 
 ####heritability
-heritability <- function(x,y){
+heritability <- function(x, y){
   library(lme4)
   library(Matrix)
-  Entry= as.factor(y$Entry)
+  Entry = as.factor(y$Entry)
   REP = as.factor(y$Rep)
-  x=as.numeric(x)
-  Misvarcomp <- lmer(x~ (1|Entry)+ (1|Rep) + (1|Year), data=y)
-  A=unlist(VarCorr(Misvarcomp))[[1]]
-  B=attr(VarCorr(Misvarcomp), "sc")^2
-  h2=A/(A+B/8)
+  x = as.numeric(x)
+  Misvarcomp <- lmer(x ~ (1|Entry) + (1|Rep) + (1|Year), data = y)
+  A = unlist(VarCorr(Misvarcomp))[[1]]
+  B = attr(VarCorr(Misvarcomp), "sc") ^ 2
+  h2 = A/(A+B/8)
   ####question: I used 8 as the number of Var_residual devided, but i am not sure, please let me know if it is not right. Thanks.
   return(h2)
 }
