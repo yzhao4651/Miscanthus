d18c6629a76cb70a0e6e23e426342e211316673f
Yongli Zhao <46494340+yzhao4651@users.noreply.github.com>
2019-01-26 20:12:25 -0600

    Add files via upload

---
+++ hetability fucntion .R
@@ -0,0 +1,22 @@
+
+### write funcion 
+####heritability
+lm=function(x,y){
+  library(lme4)
+  library(Matrix)
+  Entry= as.factor(y$Entry)
+  REP = as.factor(y$Rep)
+  x=as.numeric(x)
+  Misvarcomp <- lmer(x~ (1|Entry)+ (1|Rep) + (1|Year), data=y)
+  A=unlist(VarCorr(Misvarcomp))[[1]]
+  B=attr(VarCorr(Misvarcomp), "sc")^2
+  h2=A/(A+B/8)
+  ####question: I used 8 as the number of Var_residual devided, but i am not sure, please let me know if it is not right. Thanks.
+  return(h2)
+}
+###import the data 
+qualdat <- read.csv("~/Documents/whole traits/Copy of alltraitsflowerday.csv")
+lm(qualdat$fday,qualdat)
+
+
+
