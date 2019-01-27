49a76bc34de22fab61c13373e52fc0d5df1ed7b5
Lindsay Clark <dragonzuela04@gmail.com>
2019-01-26 22:46:10 -0600

    Add Entry:Year back in to model, after looking at SolCAP tutorial

--- hetability fucntion .R
+++ hetability fucntion .R
@@ -9,11 +9,14 @@ heritability <- function(x, y){
   Rep = as.factor(y$Rep)
   Year = as.factor(y$Year)
   
-  Misvarcomp <- lmer(x ~ (1|Entry) + (1|Rep) + (1|Year))
-  A = unlist(VarCorr(Misvarcomp))[[1]]    # variance attributable to entry
-  B = attr(VarCorr(Misvarcomp), "sc") ^ 2 # residual variance
-  lvls <- length(levels(Rep:Year))        # get the divisor for residual (8)
-  h2 = A/(A+B/lvls)
+  Misvarcomp <- lmer(x ~ (1|Entry) + (1|Rep) + (1|Year) + (1|Entry:Year))
+  A = unlist(VarCorr(Misvarcomp))[["Entry"]]         # variance attributable to entry
+  B = attr(VarCorr(Misvarcomp), "sc") ^ 2            # residual variance
+  C = unlist(VarCorr(Misvarcomp))[["Entry:Year"]]    # variance attributable to entry by year
+  lvlsB <- length(levels(Rep:Year))       # get the divisor for residual (8)
+  lvlsC <- length(levels(Year))           # get the divisor for entry by year (2)
+  
+  h2 = A/(A + B/lvlsB + C/lvlsC)
   ####question: I used 8 as the number of Var_residual devided, but i am not sure, please let me know if it is not right. Thanks.
   #### yes, 8 is correct. -LVC
   return(h2)
