eeae695f49a4055c258e8610d37492e6a795c740
Lindsay V. Clark <lvclark@illinois.edu>
2019-01-28 16:20:57 -0600

    Fix loop to put BLUPs into matrix, and add Entry:Year to model

--- Mis BLUP.R
+++ Mis BLUP.R
@@ -13,19 +13,21 @@ library(Matrix)
 out_start=4
 out_end=14
 out_nvar=out_end-out_start+1
-out_variable=rep(NA, out_nvar)
+out_variable = colnames(traits)[out_start:out_end]
+# set up a matrix to contain all BLUP values
+out_beta <- matrix(0, nrow = length(levels(traits$Entry)),
+                   ncol = out_nvar,
+                   dimnames = list(levels(traits$Entry), 
+                                   out_variable))
 number=1
 for (i in out_start:out_end){
   outcome = colnames(traits)[i]
-  model <- lmer(get(outcome)~ (1|Entry)+ (1|Rep) + (1|Year),
+  model <- lmer(get(outcome)~ (1|Entry)+ (1|Rep) + (1|Year) + (1|Entry:Year),
                 na.action = na.exclude,
                 data=traits)
   beta <- ranef(model)
-  out_beta[number] = as.numeric(beta[2])
-  out_variable[number] = outcome
+  out_beta[rownames(beta$Entry), number] = beta$Entry[[1]]
   number = number + 1
 }
-outcome = data.frame(out_variable, out_beta)
 
-
-
+# out_beta is the new output
