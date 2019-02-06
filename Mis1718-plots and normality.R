64da4ca096ef16ab2d02c0aa3c1397dee9bcca01
Lindsay V. Clark <lvclark@illinois.edu>
2019-02-06 15:58:15 -0600

    fix function indentation for readability

--- Mis1718-plots and normality.R
+++ Mis1718-plots and normality.R
@@ -44,15 +44,15 @@ pairs(~Yld_kg+SDW_kg+Cml_cm+CmD_BI_mm+CmD_LI_mm+CmN.+CmDW_g+Bcirc_cm+FNMain+FNsm
 ####out_start: the first variable for boxplot, 
 ####out_end: the last variable for boxplot
 plotboxFunc <- function(out_start,out_end, mydata, na.rm = TRUE) {
-    pdf(paste("Boxplot", 2 ,".pdf",sep="")) 
-    par(mar = c(5,4,1,1))
-    par(mfrow=c(3,3))
-    for (i in out_start:out_end){
-   boxplot(qualdat[,i] ~ qualdat$Rep, col=(c("pink")), 
-          xlab="", ylab="", main=paste("Boxplot of ", names(qualdat[i]), sep = ""), cex.main=1)
-          mtext(text = "Rep",side = 1, line = 1.5, cex=0.7)
-          mtext(text = paste("var_", names(qualdat[i]), sep = ""), side = 2, line = 2,cex=0.7) #side 2 = left
-    }
+  pdf(paste("Boxplot", 2 ,".pdf",sep="")) 
+  par(mar = c(5,4,1,1))
+  par(mfrow=c(3,3))
+  for (i in out_start:out_end){
+    boxplot(qualdat[,i] ~ qualdat$Rep, col=(c("pink")), 
+            xlab="", ylab="", main=paste("Boxplot of ", names(qualdat[i]), sep = ""), cex.main=1)
+    mtext(text = "Rep",side = 1, line = 1.5, cex=0.7)
+    mtext(text = paste("var_", names(qualdat[i]), sep = ""), side = 2, line = 2,cex=0.7) #side 2 = left
+  }
   dev.off()
 }
 plotboxFunc(7,27,qualdat)
