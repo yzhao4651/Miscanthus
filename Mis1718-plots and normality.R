5d3c993671bafc6ea84554a0aa3644fde9bb2294
Yongli Zhao <yzhao4651@tuskegee.edu>
2019-02-06 15:28:47 -0600

    just check

--- Mis1718-plots and normality.R
+++ Mis1718-plots and normality.R
@@ -102,7 +102,8 @@ for(j in 2: nrow(lda))
   return(x[i])
 }
 
-### becasue this function i wrote above does not work, I still write one by one to get the transformation data
+### becasue this function above does not work, I still write one by one to get the transformation data,
+## do you have any idea for this one, It is really hard for me to do this one 
 ### Lindsay's code to get the transformed data
 bc <- function(x, lda){ # function to transform data after lambda is determined
   while(min(x, na.rm = TRUE) <= 0){
