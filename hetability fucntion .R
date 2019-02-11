05c180e4f42cd8c1a6620850c9e4105ddaa0cf3c
Yongli Zhao <yzhao4651@tuskegee.edu>
2019-02-10 22:13:54 -0600

    I changed this R codes with function, loop, and condition in order to get the heritablity of all traits even with only one years data.

--- hetability fucntion .R
+++ hetability fucntion .R
@@ -1,30 +1,81 @@
+###import the data
+###import the data
+normadata <- read.csv("~/Documents/whole traits/traits1718normalited1.csv",na.strings = c("",".","NA"))
+###check the data format
+str(normadata)
+###change the format of the several variables 
+normadata$GS <- as.numeric(as.character(normadata$GS))
+normadata$SRD <- as.numeric(as.character(normadata$SRD))
+normadata$Entry=as.factor(normadata$Entry)
+normadata$Rep=as.factor(normadata$Rep)
+normadata$Year=as.factor(normadata$Year)
+### hetability for two years 
+normdata_TY <- normadata[,c(4:12,14:17,22:23,28)]
+###check the data format
+str(normdata_TY)
 
-### write funcion 
-####heritability
-heritability <- function(x, y){
+heritability <- function(out_start,out_end,y){
   require(lme4)
   require(Matrix)
-  
   Entry = as.factor(y$Entry)
   Rep = as.factor(y$Rep)
   Year = as.factor(y$Year)
-  
-  Misvarcomp <- lmer(x ~ (1|Entry) + (1|Rep) + (1|Year) + (1|Entry:Year))
-  A = unlist(VarCorr(Misvarcomp))[["Entry"]]         # variance attributable to entry
-  B = attr(VarCorr(Misvarcomp), "sc") ^ 2            # residual variance
-  C = unlist(VarCorr(Misvarcomp))[["Entry:Year"]]    # variance attributable to entry by year
-  lvlsB <- length(levels(Rep:Year))       # get the divisor for residual (8)
-  lvlsC <- length(levels(Year))           # get the divisor for entry by year (2)
-  
-  h2 = A/(A + B/lvlsB + C/lvlsC)
-  ####question: I used 8 as the number of Var_residual devided, but i am not sure, please let me know if it is not right. Thanks.
-  #### yes, 8 is correct. -LVC
-  return(h2)
+  out_nvar=out_end-out_start+1
+  out_variable = colnames(y[out_start:out_end])
+  outcome <- matrix(0, nrow=ncol(y[out_start:out_end]),
+                    ncol =1, dimnames = list(out_variable,c(paste(c("Heritability")))))
+  number=1
+  for (i in out_start:out_end){
+    Misvarcomp<- lmer(y[,i] ~ (1|Entry) + (1|Rep) + (1|Year) + (1|Entry:Year))
+    A= unlist(VarCorr(Misvarcomp))[["Entry"]]         # variance attributable to entry
+    B= attr(VarCorr(Misvarcomp), "sc") ^ 2            # residual variance
+    C = unlist(VarCorr(Misvarcomp))[["Entry:Year"]]    # variance attributable to entry by year
+    lvlsB <- length(levels(Rep:Year))       # get the divisor for residual (8)
+    lvlsC <- length(levels(Year))           # get the divisor for entry by year (2)
+    h2 = A/(A+ B/lvlsB + C/lvlsC)
+    outcome[number] <-h2 
+    number=number+1
+  }
+  return(outcome)
 }
-# ###import the data 
-# qualdat <- read.csv("Copy of alltraitsflowerday.csv", na.strings = ".")
-# heritability(qualdat$fday,qualdat)
-# 
-# # for debugging
-# x <- qualdat$fday
-# y <- qualdat
+
+Heritwoyear <- heritability(5,16,normdata_TY)
+write.csv(Heritwoyear, file = "~/Documents/whole traits/Heritwoyear.csv", row.names = T, na = ".")
+
+####heritablity for all of the varialbes  
+heritability <- function(out_start,out_end,y){
+  require(lme4)
+  require(Matrix)
+  Entry = as.factor(y$Entry)
+  Rep = as.factor(y$Rep)
+  Year = as.factor(y$Year)
+  out_nvar=out_end-out_start+1
+  out_variable = colnames(y[out_start:out_end])
+  outcome <- matrix(0, nrow=ncol(y[out_start:out_end]),
+                    ncol =1, dimnames = list(out_variable,c(paste(c("Heritability")))))
+  number=1
+  for (i in out_start:out_end){
+    if(sum(is.na(y[y$Year==2017 | y$Year==2018,][i])) >= 900){
+      Misvarcomp<- lmer(y[,i] ~ (1|Entry) + (1|Rep))
+      A= unlist(VarCorr(Misvarcomp))[["Entry"]]         # variance attributable to entry
+      B= attr(VarCorr(Misvarcomp), "sc") ^ 2            # residual variance
+      lvlsB <- length(levels(Rep))       # get the divisor for residual (8)
+      h2 = A/(A+ B/lvlsB)
+    } else {
+    Misvarcomp<- lmer(y[,i] ~ (1|Entry) + (1|Rep) + (1|Year) + (1|Entry:Year))
+    A= unlist(VarCorr(Misvarcomp))[["Entry"]]         # variance attributable to entry
+    B= attr(VarCorr(Misvarcomp), "sc") ^ 2            # residual variance
+    C = unlist(VarCorr(Misvarcomp))[["Entry:Year"]]    # variance attributable to entry by year
+    lvlsB <- length(levels(Rep:Year))       # get the divisor for residual (8)
+    lvlsC <- length(levels(Year))           # get the divisor for entry by year (2)
+    h2 = A/(A+ B/lvlsB + C/lvlsC)
+    }
+    outcome[number] <-h2 
+    number=number+1
+ }
+  return(outcome)
+}
+Herit <- heritability(8,28,normadata)
+write.csv(Herit, file = "~/Documents/whole traits/heritabilityall.csv", row.names = T, na = ".")
+
+
