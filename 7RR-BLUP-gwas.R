8f3410a02f7d16f6129d09a7469803e028de16c2
Yongli Zhao <yzhao4651@tuskegee.edu>
2019-03-10 11:14:33 -0500

    R codes using rrBLUP, quesitons in the file

--- 7RR-BLUP-gwas.R
+++ 7RR-BLUP-gwas.R
@@ -4,18 +4,15 @@ myY <- read.csv("data/myYimputedSNP19.csv")
 myGD <- read.csv("data/myGDimputedSNP19.csv",row.names=1)
 myGM <- read.csv("data/myGMimputedSNP19.csv")
 myQ<- read.csv("data/myQimputedSNP19.csv",row.names=1)
-
 ###tansform the genotype data set into rrblup data set
 myGDt <- t(myGD)-1
 #myGDtdataframe <- data.frame(myGDt)
 #head(myGDtdataframe)
-
 ### change the row name into the first column 
 myGDtdataframe <- data.frame(myGDt)
 Name <- rownames(myGDtdataframe)
 rownames(myGDtdataframe) <- NULL
 myGDtdataframe <- cbind(Name,myGDtdataframe)
-
 ###combine the genotype with the chromosome position (dataset: myGM)
 genorrblup <- merge(myGM,myGDtdataframe,by="Name")
 ###change the ID of the name of phenotype to same to the ID of the genotype ( like change "_" to "." )
@@ -28,10 +25,8 @@ str(myY)
 ###run rrblup
 install.packages("rrBLUP")
 
-
 ###1:question, I can not open the pdf, even opened it, only one page show image. 
 ###I tried get off the pdf and dev.off() and run again, but the plot did not show the Plots area. 
- 
 ###2: how to output the result in to csv file? 
 ###3: I set up pdf(paste("Plots from rrBLUP", 1, ".pdf", sep="")), 
 ###but i changed the number with 2 next time like pdf(paste("Plots from rrBLUP", 2, ".pdf", sep="")), 
@@ -44,7 +39,6 @@ par(mfrow=c(25,2))
 GWAS(myY[1:2], genorrblup, fixed=NULL, K=NULL, n.PC=0,
      min.MAF=0.05, n.core=1, P3D=TRUE, plot=TRUE)
 dev.off()
-
 
 ###tryig to calculate the kinship 
 ###change the first column into row name
