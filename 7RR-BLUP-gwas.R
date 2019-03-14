483e0b86df151c6efb66c853f9d276ae632303a2
Lindsay V. Clark <lvclark@illinois.edu>
2019-03-14 16:47:03 -0500

    A.mat expects matrix, so convert input to matrix

--- 7RR-BLUP-gwas.R
+++ 7RR-BLUP-gwas.R
@@ -63,7 +63,7 @@ dev.off()
 
 ###tryig to calculate the kinship 
 ###change the first column into row name
-myGDk <- myGD - 1
+myGDk <- as.matrix(myGD - 1)
 dimnames(myGDk)[[1]]<- gsub("-",".",dimnames(myGDk)[[1]])
 kmatrix <- A.mat(myGDk,min.MAF=NULL,max.missing=NULL,impute.method="mean",tol=0.02,
       n.core=1,shrink=FALSE,return.imputed=FALSE)
