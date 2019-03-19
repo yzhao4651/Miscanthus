5ad70b43e0667cfa7c0b06db2983648590cfed1b
Lindsay V. Clark <lvclark@illinois.edu>
2019-03-19 13:44:34 -0500

    Make an import script to avoid repetitive code and make things easier to rerun.

--- 1Descriptive statistiscs and Normality.R
+++ 1Descriptive statistiscs and Normality.R
@@ -1,41 +1,6 @@
 ####import the data####
-library(readr)
-qualdat  <- read.csv("~/Documents/whole traits/Copy of trait1718-8.csv" , na.strings = c("",".","NA"))
-qualdat  <- read.csv("data/Copy of trait1718-8.csv" , na.strings = c("",".","NA")) # on Yongli's computer
-# mywd <- "." # on Lindsay's computer
-qualdat <- read.csv(file.path(mywd, "Copy of trait1718-4.csv"), na.strings = c("",".","NA"))
-#check the data formate
-str(qualdat)
-###change character of Growth Stage to number N=1,B=2,F=3,P=4
-levels(qualdat$GS)[levels(qualdat$GS)=="N"] <- "1"
-levels(qualdat$GS)[levels(qualdat$GS)=="n"] <- "1"
-levels(qualdat$GS)[levels(qualdat$GS)=="B"] <- "2"
-levels(qualdat$GS)[levels(qualdat$GS)=="b"] <- "2"
-levels(qualdat$GS)[levels(qualdat$GS)=="F"] <- "3"
-levels(qualdat$GS)[levels(qualdat$GS)=="p"] <- "4"
-levels(qualdat$GS)[levels(qualdat$GS)=="P"] <- "4"
-####calculate days 
-qualdat$SRD <- as.numeric(as.Date(qualdat$SRD,format = "%m/%d/%Y")-as.Date(qualdat$datest2,format = "%m/%d/%Y"))
-qualdat$ADD <-as.numeric(as.Date(qualdat$ADD,format = "%m/%d/%Y")-as.Date(qualdat$datest2,format = "%m/%d/%Y"))
-qualdat$HD_1 <- as.numeric(as.Date(qualdat$HD_1,format = "%m/%d/%Y")-as.Date(qualdat$datest1,format = "%m/%d/%Y"))
-qualdat$FD_1 <-as.numeric(as.Date(qualdat$FD_1,format = "%m/%d/%Y")-as.Date(qualdat$datest1,format = "%m/%d/%Y"))
-qualdat$HD_50. <- as.numeric(as.Date(qualdat$HD_50.,format = "%m/%d/%Y")-as.Date(qualdat$datest1,format = "%m/%d/%Y"))
-qualdat$FD_50. <- as.numeric(as.Date(qualdat$FD_50.,format = "%m/%d/%Y")-as.Date(qualdat$datest1,format = "%m/%d/%Y"))
-####check the data formate
-str(qualdat)
-###select the data need for analysis 
-qualdat <- qualdat[,c(3,5:6,4,7:27)]
-####check the data formate
-str(qualdat)
-#### change several variables numeric format
-# Note from Lindsay -- converting from integer to float is unnecessary, but should not cause problems
-indx <- sapply(qualdat[,c(16,19:23)], is.integer)
-qualdat[,c(16,19:23)][indx] <- lapply(qualdat[,c(16,19:23)][indx], function(x) as.numeric(as.character(x)))
-###change several variables format
-qualdat$GS <- as.numeric(as.character(qualdat$GS))
-qualdat$Entry=as.factor(qualdat$Entry)
-qualdat$Rep=as.factor(qualdat$Rep)
-qualdat$Year=as.factor(qualdat$Year)
+source("0 Qualdat import.R")
+
 ###save this data set 
 save(qualdat,file="qualdat.RData")
 ####check the data formate
