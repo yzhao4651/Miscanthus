6e6e9fda1b78a1ade153927e7084f91d2a5f33fa
Yongli Zhao <yzhao4651@tuskegee.edu>
2019-03-26 13:17:55 -0500

    Function for boxlot VS Year

--- BoxplotFunVSYear.R
+++ BoxplotFunVSYear.R
@@ -5,16 +5,14 @@
 ####in front of the function when citing this function. 
 ####In this way, the number 1 can be changed to the different number when using different variables 
 #### in case the file will be overlap every time to use
-plotboxFunc <- function(out_start,out_end, mydata, na.rm = TRUE) {
+plotboxFuncVSYear <- function(out_start,out_end, mydata, na.rm = TRUE) {
   par(mar = c(5,4,1,1))
   par(mfrow=c(3,3))
   for (i in out_start:out_end){
-    boxplot(mydata[,i] ~ mydata$Rep, col=(c("pink")), 
+    boxplot(mydata[,i] ~ mydata$Year, col=(c("pink")), 
             xlab="", ylab="", main=paste("Boxplot of ", names(mydata[i]), sep = ""), cex.main=1)
     mtext(text = "Rep",side = 1, line = 1.5, cex=0.7)
     mtext(text = paste("var_", names(mydata[i]), sep = ""), side = 2, line = 2,cex=0.7) #side 2 = left
   }
-dev.off()
+  dev.off()
 }
-
-
