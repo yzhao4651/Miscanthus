51f8c67ddafc97be5a1b705e0d1fc7b3c07a9907
Yongli Zhao <yzhao4651@tuskegee.edu>
2019-03-28 17:34:56 -0500

    updated file in R

--- Function/getmyGM.R
+++ Function/getmyGM.R
@@ -1,5 +1,5 @@
 getmyGM <- function(myGM,myGD){
-  n <- data.frame(t(data.frame(myGD.clum.107.3707,row.names = 1)))
+  n <- data.frame(t(data.frame(myGD,row.names = 1)))
   myGM2 <- myGM[match(dimnames(n)[[1]],myGM$Name, nomatch=0),]
   snporder <- order(myGM2$Chromosome, myGM2$Position)
   myGM2 <- myGM2[snporder,]
