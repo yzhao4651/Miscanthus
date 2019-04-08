da1bfebd1f46ec036a38ffbb1fa54aed1a880342
yzhao4651 <yzhao4651@tuskegee.edu>
2019-04-08 12:49:36 -0500

    R codes for running the flowering traits using mrMLM software

--- 8-2 Flo-int-runall-mrMLMM.R
+++ 8-2 Flo-int-runall-mrMLMM.R
@@ -1,5 +1,4 @@
 ###change some number in the genotype to int
-
 subgenomrMLMM <- read.csv("data/subgenomrMLMM.csv")
 subgenomrMLMM1 <- data.frame(subgenomrMLMM[,c(1,5:157)],row.names = 1)
 ###seperate two parts
@@ -8,7 +7,6 @@ subgenomrMLMMtran[subgenomrMLMMtran < 1 & subgenomrMLMMtran > 0.5] <- 1
 subgenomrMLMMtran[subgenomrMLMMtran < 0.5 & subgenomrMLMMtran > 0] <- 0
 subgenomrMLMMtran[subgenomrMLMMtran < -0.5 & subgenomrMLMMtran > -1] <- -1
 subgenomrMLMMtran[subgenomrMLMMtran < 0 & subgenomrMLMMtran > -0.5] <- 0
-
 ###flowering traits
 ###flowering traits
 mymrMLMMflo <- read.csv("mrMLMM2/myYmrMlMMflo3.csv")
@@ -41,3 +39,38 @@ mrMLM(fileGen="C:\\Users\\Admin\\Desktop\\Miscanthus\\Miscanthus\\mrMLMM2\\subge
       SearchRadius=20,CriLOD=3,SelectVariable=50,
       Bootstrap=FALSE,DrawPlot=FALSE,
       Plotformat ="jpeg",Resolution="Low", dir= "mrMLMM2/Resultfloall3")
+
+###using the one with imputed number in SNPs datasets
+subgenomrMLMM <- read.csv("data/subgenomrMLMM.csv")
+str(subgenomrMLMM)
+subgenomrMLMM1 <- data.frame(subgenomrMLMM[,c(1,5:157)],row.names = 1)
+subgenomrMLMMtran <- data.frame(t(subgenomrMLMM1))
+str(subgenomrMLMMtran)
+#mymrMLMMflo <- read.csv("data/myYmrMLMMflo.csv")# this one has no any missing values 
+mymrMLMMflo <- read.csv("mrMLMM2/myYmrMLMMflo3.csv")# this one has two more column and then has missing values 
+str(mymrMLMMflo)
+###get the same
+genomymrMLMMflo <-subgenomrMLMMtran[match(mymrMLMMflo$X.phenotype.,rownames(subgenomrMLMMtran), nomatch=0),]
+###select the MAF>0.01
+source("Function/SelectMAF-mrMLMM.R")
+genomymrMLMMflo <- Select.MAF(genomymrMLMMflo)
+str(genomymrMLMMflo)
+##get the mateched SNP 
+genomymrMLMMflo <-subgenomrMLMM[match(genomymrMLMMflo$rn,subgenomrMLMM$rs., nomatch=0),]
+str(genomymrMLMMflo)
+###change the name in order to fit the software requirment
+colnames(genomymrMLMMflo)[which(names(genomymrMLMMflo) == "rs.")] <- "rs#"
+colnames(genomymrMLMMflo)[which(names(genomymrMLMMflo) == "genotype.for.code.1")] <- "genotype for code 1"
+###write out the dataset
+write.csv(genomymrMLMMflo, file = "mrMLMM2/subgenomrMLMMflo.csv", row.names = FALSE, na = "NA")
+
+library("mrMLM")
+mrMLM(fileGen="C:\\Users\\Admin\\Desktop\\Miscanthus\\Miscanthus\\mrMLMM2\\subgenomrMLMMflo.csv",
+      filePhe="C:\\Users\\Admin\\Desktop\\Miscanthus\\Miscanthus\\mrMLMM2\\myYmrMlMMflo3.csv",
+      fileKin=NULL,filePS=NULL,Genformat="Num",
+      method=c("mrMLM","FASTmrMLM","FASTmrEMMA","pLARmEB","pKWmEB","ISIS EM-BLASSO"),
+      Likelihood="REML",
+      trait=1:23,
+      SearchRadius=20,CriLOD=3,SelectVariable=50,
+      Bootstrap=FALSE,DrawPlot=FALSE,
+      Plotformat ="jpeg",Resolution="Low", dir= "mrMLMM2/Resultfloall1")
