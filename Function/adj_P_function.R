733abe077a4df4e5b3a03927b907fdfbb9324629
yzhao4651 <yzhao4651@tuskegee.edu>
2019-06-07 10:16:19 -0500

    updated files

---
+++ Function/adj_P_function.R
@@ -0,0 +1,6 @@
+adj_P_function <- function(data,start_var, end_var,na.rm=TRUE){
+  for(i in start_var:end_var){
+    data[ ,paste0("Adj.P.",colnames(data[i]))] <- p.adjust(data[[i]], method ="fdr", n = length(data[[i]]))
+  }
+  return(data)
+}
